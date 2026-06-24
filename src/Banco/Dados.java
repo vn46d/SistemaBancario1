7e765dd8ffb0af1e1695ad63839b88f89fbd3c7a
vN46 <viniciussa171l@gmail.com>
2026-06-23 22:49:42 -0300

    Valores Indicados e Switch (saldo,deposito)

--- src/Banco/Dados.java
+++ src/Banco/Dados.java
@@ -1,17 +1,55 @@
 package Banco;
 
-import java.security.DrbgParameters;
 import java.util.Scanner;
 
     public class Dados {
         public static void main(String[] args) {
+
+            Scanner option = new Scanner(System.in);
+
+
+            String nomedoTitular;
+            String sel;
+            Double saldo;
+            Double deposito;
+            Double sacar;
+
             System.out.println(" ======= Banco Do Vinicius ======== ");
 
-            Scanner nome = new Scanner(System.in);
-            String nomedoTitular;
-
             System.out.println("Digite seu nome: ");
-            nomedoTitular = nome.nextLine();
+            nomedoTitular = option.nextLine();
             System.out.println("Bem-Vindo , " + nomedoTitular + "!");
+
+            System.out.println("Digite uma dos numeros : 1 ,2 ,3 ");
+            sel = option.nextLine();
+
+            switch (sel) {
+                case "Adiconar" :
+                    System.out.println("Adicione o valor Desejado");
+                    saldo = option.nextDouble();
+
+                    saldo = saldo + deposito;
+
+                    System.out.println("Saldo atualizado: R$ " + saldo);
+
+                case "1":
+                    System.out.println("1 - " + saldo);
+                    break;
+                case "2":
+                    System.out.println("2 - " + deposito);
+                    deposito = option.nextDouble();
+                    break;
+                case "3":
+                    System.out.println("3 - " + sacar);
+                    sacar = option.nextDouble();
+                    break;
+            if saldo <= sacar : {
+                    System.out.println("Saldo insuficiente");
+                }
+                default:
+                    System.out.println("Operação inválida");
+                    break;
+            }
+
+        }
     }
-}
