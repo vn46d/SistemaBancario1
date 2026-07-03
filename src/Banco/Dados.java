3152cbbd4bd27bb0ba3b1f04133776456a027b95
vN46 <viniciussa171l@gmail.com>
2026-07-02 23:04:37 -0300

    Alteracao de erros(switch,sintaxe,redondo)

--- src/Banco/Dados.java
+++ src/Banco/Dados.java
@@ -2,54 +2,66 @@ package Banco;
 
 import java.util.Scanner;
 
-    public class Dados {
-        public static void main(String[] args) {
+public class Dados {
+    public static void main(String[] args) {
 
-            Scanner option = new Scanner(System.in);
+        System.out.println(" ======= Banco Do Vinicius ======== ");
+
+        Scanner option = new Scanner(System.in);
 
 
-            String nomedoTitular;
-            String sel;
-            Double saldo;
-            Double deposito;
-            Double sacar;
+        String nomedoTitular;
+        String sel;
+        Double saldo;
+        Double deposito;
+        Double sacar;
 
-            System.out.println(" ======= Banco Do Vinicius ======== ");
+        deposito = 0;
+        saldo = 0;
+        sacar = 0;
 
-            System.out.println("Digite seu nome: ");
-            nomedoTitular = option.nextLine();
-            System.out.println("Bem-Vindo , " + nomedoTitular + "!");
+        System.out.println("Digite seu nome: ");
+        nomedoTitular = option.nextLine();
 
-            System.out.println("Digite uma dos numeros : 1 ,2 ,3 ");
-            sel = option.nextLine();
+        System.out.println("Bem-Vindo , " + nomedoTitular + "!");
 
-            switch (sel) {
-                case "Adiconar" :
-                    System.out.println("Adicione o valor Desejado");
-                    saldo = option.nextDouble();
+        System.out.println("Digite uma dos numeros : 1 ,2 ,3 ");
 
-                    saldo = saldo + deposito;
 
-                    System.out.println("Saldo atualizado: R$ " + saldo);
+        sel = option.nextLine();
 
-                case "1":
-                    System.out.println("1 - " + saldo);
+        switch (sel) {
+
+            case "1":
+                System.out.println("1 - " + saldo);
+
+
+                System.out.println("Saldo atualizado: R$ " + saldo);
                     break;
-                case "2":
-                    System.out.println("2 - " + deposito);
-                    deposito = option.nextDouble();
+
+            case "2":
+                System.out.println("Digite o valor do depósito:" + deposito);
+                deposito = option.nextDouble();
+                saldo = saldo + deposito;
+                System.out.println("O saldo adicionado a sua conta foi de" + deposito);
+
                     break;
-                case "3":
-                    System.out.println("3 - " + sacar);
-                    sacar = option.nextDouble();
-                    break;
-            if saldo <= sacar : {
+
+            case "3":
+                System.out.println("3 - " + sacar);
+                sacar = option.nextDouble();
+
+                if (saldo >= sacar) {
+                    saldo = saldo - sacar;
+                    System.out.println("Restante" + saldo);
+                } else {
                     System.out.println("Saldo insuficiente");
-                }
-                default:
-                    System.out.println("Operação inválida");
                     break;
-            }
+                }
 
+            default:
+                     System.out.println("Operação inválida");
+                    break;
         }
     }
+}
